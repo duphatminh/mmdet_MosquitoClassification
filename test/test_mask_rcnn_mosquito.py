b046f64e1d51ede708aa97b91b4ff79007650af0
duphatminh <duphatminhh@gmail.com>
2024-07-04 10:43:19 +0700

    Update Unit Test

--- test/test_mask_rcnn_mosquito.py
+++ test/test_mask_rcnn_mosquito.py
@@ -2,9 +2,7 @@ import unittest
 from mmdet.apis import init_detector, inference_detector
 class TestMMDetection(unittest.TestCase):
 
-    #
-    def test_predict_01(self):
-
+    def test_predict_model(self):
         config_file = '../configs/mosquito_model/mask-rcnn_r50-caffe_fpn_ms-poly-1x_mosquito.py'
         checkpoint_file = 'mark_rcnn_mosquito.pth'
         model = init_detector(config_file, checkpoint_file, device='cuda:0')  # device='cpu' or device='cuda:0'
