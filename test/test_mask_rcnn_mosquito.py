d4d16ef9c68f426fc3164099b6b18b17f8bc4278
duphatminh <duphatminhh@gmail.com>
2024-07-04 10:17:58 +0700

    Update Unit Test mark_rcnn_mosquito model

--- test/test_mask_rcnn_mosquito.py
+++ test/test_mask_rcnn_mosquito.py
@@ -1,36 +1,15 @@
 import unittest
-import torch
+from mmdet.apis import init_detector, inference_detector
+class TestMMDetection(unittest.TestCase):
 
-class TestStringMethods(unittest.TestCase):
+    #
+    def test_predict_01(self):
 
-    def test_found_object(self):
-        model_fpath = '../test/mark_rcnn_mosquito.pth'
-        model = torch.jit.load(model_fpath)
-
-        image_fpath = '../test/anopheles.jpg'
-        result = model(image_fpath)
-
-        self.assertIsNotNone(result)
-
-        list_results = list(result)
-        self.assertIsNone(list_results[0])
-
-        first_result = list_results[0]
-        print(f"Names in the model: {first_result.names()}")
-
-        list_boxes = list(first_result.boxes)
-        box = list_boxes[0]
-
-        index_object = int(box.cls[0])
-
-        self.assertEqual(index_object, 0)
-
-        detected_classname = first_result.names[index_object]
-        self.assertEqual(detected_classname, '00-aedes-dau')
-
-        self.assertGreater(box.conf[0], 0.5)
-
-        first_result.save(filename='output.jpg')
+        config_file = '../configs/mosquito_model/mask-rcnn_r50-caffe_fpn_ms-poly-1x_mosquito.py'
+        checkpoint_file = 'mark_rcnn_mosquito.pth'
+        model = init_detector(config_file, checkpoint_file, device='cuda:0')  # device='cpu' or device='cuda:0'
+        inference_detector(model, '../data/mosquito/MyInference/aedes.jpg')
+        print('Done')
 
 if __name__ == '__main__':
     unittest.main()
